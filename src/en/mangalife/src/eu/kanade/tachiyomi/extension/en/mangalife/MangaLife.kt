6bf82744b87d78d2f9050a5a437b0000090f0fe7
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-10-13 22:45:50 +0100

    update

--- src/en/mangalife/src/eu/kanade/tachiyomi/extension/en/mangalife/MangaLife.kt
+++ src/en/mangalife/src/eu/kanade/tachiyomi/extension/en/mangalife/MangaLife.kt
@@ -40,7 +40,7 @@ class MangaLife : HttpSource() {
 
     override val supportsLatest = true
 
-    var coverUrlPlaceholder: String
+    var coverUrlPlaceholder: String? = null
 
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
         .connectTimeout(1, TimeUnit.MINUTES)
